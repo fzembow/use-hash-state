4d8e36ea978127c62198cd32d98a9539a6da9e75
Fil Zembowicz <fil@formsort.com>
2020-04-17 21:51:54 -0400

    Add config object, allow specifying to use pushState

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
   ],
+  rules: {
+    "@typescript-eslint/interface-name-prefix": "off"
+  }
 };
--- src/use-hash-state.ts
+++ src/use-hash-state.ts
@@ -1,5 +1,11 @@
-import { useRef, useState } from 'react';
-import { debounce } from "ts-debounce";
+import { useRef, useState, useEffect } from 'react';
+import { debounce } from 'ts-debounce';
+
+interface UseHashStateOptions {
+  usePushState?: boolean;
+}
+
+const DEBOUNCE_WRITE_URL_MS = 250;
 
 const parseStateFromUrl = <T extends object>(): T | undefined => {
   const hash = window.location.hash.slice(1);
@@ -14,19 +20,26 @@ const parseStateFromUrl = <T extends object>(): T | undefined => {
   }
 };
 
-const writeStateToUrl = debounce((newState: unknown): void => {
-  const json = JSON.stringify(newState);
-  history.replaceState(
-    undefined,
-    document.title,
-    window.location.pathname + '#' + encodeURIComponent(json)
-  );
-}, undefined, {
-  isImmediate: true
-});
+const writeStateToUrl = debounce(
+  (newState: unknown, usePushState = false): void => {
+    const json = JSON.stringify(newState);
+    const { title } = document;
+    const url = `${window.location.pathname}#${encodeURIComponent(json)}`;
+    if (usePushState) {
+      history.pushState(undefined, title, url);
+    } else {
+      history.replaceState(undefined, title, url);
+    }
+  },
+  DEBOUNCE_WRITE_URL_MS,
+  {
+    isImmediate: true,
+  }
+);
 
 const useHashState = <T extends object>(
-  initialState: T
+  initialState: T,
+  { usePushState }: UseHashStateOptions = { usePushState: false }
 ): [T, (key: keyof T, value: unknown) => void] => {
   const didRender = useRef<boolean>(false);
 
@@ -36,20 +49,35 @@ const useHashState = <T extends object>(
     if (parsedState) {
       initialState = parsedState;
     } else if (initialState) {
-      writeStateToUrl(initialState);
+      writeStateToUrl(initialState, usePushState);
     }
     didRender.current = true;
   }
 
   const [state, setState] = useState<T>(initialState);
 
+  const onHashChange = (): void => {
+    const parsedState = parseStateFromUrl<T>();
+    if (parsedState) {
+      setState(parsedState);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('hashchange', onHashChange);
+    return (): void => {
+      window.removeEventListener('hashchange', onHashChange);
+    };
+  }, []);
+
   const setStateAtKey = (key: keyof T, value: unknown): void => {
-    setState(prevState => {
+    setState((prevState) => {
       const newState = {
         ...prevState,
         [key]: value,
       };
-      writeStateToUrl(newState);
+
+      writeStateToUrl(newState, usePushState);
       return newState;
     });
   };
